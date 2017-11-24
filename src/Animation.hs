fd568962ab5497c4fb27ba09354a612322eeb180
Olivier Sohn <olivier.sohn@gmail.com>
2017-11-25 00:39:36 +0100

    simplify

--- src/Animation.hs
+++ src/Animation.hs
@@ -19,16 +19,15 @@ import           Control.Exception( assert )
 
 import           Geo( Coords
                     , polyExtremities )
-import           WorldSize(Location(..))
 
 
 data Animation = Animation {
-    _animationRender :: !(Animation -> (Coords -> Location) -> IO (Maybe Animation))
+    _animationRender :: !(Animation  -> IO (Maybe Animation))
 }
 
-mkAnimation :: (Animation -> (Coords -> Location) -> IO (Maybe Animation))
+mkAnimation :: (Animation  -> IO (Maybe Animation))
             -> Animation
-mkAnimation render = Animation render
+mkAnimation = Animation
 
 -- \ This datastructure is used to keep a state of the animation progress, not globally,
 --   but locally on each animation point. It is also recursive, so that we can sequence
@@ -47,29 +46,27 @@ mkAnimationTree c = Tree c Nothing
 
 combine :: [Coords]
         -> [Either Tree Coords]
-        -> (Coords -> Location)
         -> [Either Tree Coords]
-combine points uncheckedPreviousState getLocation =
+combine points uncheckedPreviousState =
   let previousState = assert (length points == length uncheckedPreviousState) uncheckedPreviousState
-  in zipWith (combinePoints getLocation) points previousState
+  in zipWith combinePoints points previousState
 
-combinePoints :: (Coords -> Location)
-              -> Coords
+combinePoints :: Coords
               -> Either Tree Coords
               -> Either Tree Coords
-combinePoints _ point =
+combinePoints point =
   either Left (\_ -> Right point)
 
 applyAnimation :: (Coords -> [Coords])
-               -> (Coords -> Location)
+
                -> Tree
                -> Tree
-applyAnimation animation getLocation (Tree root branches) =
+applyAnimation animation (Tree root branches) =
   let points = animation root
-      previousState = fromMaybe (replicate (length points) $ Right $ assert (getLocation root == InsideWorld) root) branches
+      previousState = fromMaybe (replicate (length points) $ Right root) branches
       -- if previousState contains only Left(s), the animation does not need to be computed.
       -- I wonder if lazyness takes care of that or not?
-      newBranches = combine points previousState getLocation
+      newBranches = combine points previousState
   in Tree root $ Just newBranches
 
 animateNumberPure :: Int -> Coords -> [Coords]
@@ -82,29 +79,28 @@ animateNumberPure nSides _ =
 -- IO
 --------------------------------------------------------------------------------
 
-renderAnimation :: (Coords -> Location) -> Animation -> IO ()
-renderAnimation getLocation a@(Animation render) =
-    void( render a getLocation )
+renderAnimation :: Animation -> IO ()
+renderAnimation a@(Animation render) =
+    void( render a )
 
 setRender :: Animation
-          -> (Animation -> (Coords -> Location) -> IO (Maybe Animation))
+          -> (Animation  -> IO (Maybe Animation))
           -> Animation
 setRender (Animation _) = Animation
 
-animatedNumber :: Int -> Tree -> Animation -> (Coords -> Location) -> IO (Maybe Animation)
+animatedNumber :: Int -> Tree -> Animation  -> IO (Maybe Animation)
 animatedNumber n =
   animate' (mkAnimator animateNumberPure animatedNumber n)
 
 data Animator a = Animator {
-    _animatorPure :: !((Coords -> Location) -> Tree -> Tree)
-  , _animatorIO   :: !(Tree -> Animation -> (Coords -> Location) -> IO (Maybe Animation))
+    _animatorPure :: !(Tree -> Tree)
+  , _animatorIO   :: !(Tree -> Animation  -> IO (Maybe Animation))
 }
 
 mkAnimator :: (t -> Coords -> [Coords])
            -> (t
                -> Tree
                -> Animation
-               -> (Coords -> Location)
                -> IO (Maybe Animation))
            -> t
            -> Animator a
@@ -112,14 +108,14 @@ mkAnimator pure_ io_ params = Animator (applyAnimation (pure_ params)) (io_ para
 
 -- when inlining this function the problem disappears
 --{-# INLINE animate' #-}
-animate' :: Animator a -> Tree -> Animation -> (Coords -> Location) -> IO (Maybe Animation)
+animate' :: Animator a -> Tree -> Animation  -> IO (Maybe Animation)
 animate' (Animator pure_ io_) = animate pure_ io_
 
-animate :: ((Coords -> Location) -> Tree -> Tree)
+animate :: (Tree -> Tree)
         -- ^ the pure animation function
-        -> (Tree -> Animation -> (Coords -> Location) -> IO (Maybe Animation))
+        -> (Tree -> Animation  -> IO (Maybe Animation))
         -- ^ the IO animation function
-        ->  Tree -> Animation -> (Coords -> Location) -> IO (Maybe Animation)
-animate pureAnim ioAnim state a@(Animation _) getLocation = do
-  let newState = pureAnim getLocation state
+        ->  Tree -> Animation  -> IO (Maybe Animation)
+animate pureAnim ioAnim state a@(Animation _) = do
+  let newState = pureAnim state
   return $ Just (setRender a $ ioAnim newState)
--- src/Lib.hs
+++ src/Lib.hs
@@ -8,13 +8,11 @@ import           Imajuscule.Prelude
 
 import Geo(zeroCoords)
 import Animation(mkAnimation, renderAnimation, animatedNumber, mkAnimationTree)
-import WorldSize(Location(..))
 
 run :: IO ()
 run = do
   let anim = mkAnimation (animatedNumber 1 (mkAnimationTree zeroCoords))
-      fLoc _ = InsideWorld
   putStrLn "Before rendering animations"
-  _ <- renderAnimation fLoc anim
+  _ <- renderAnimation anim
   putStrLn "After rendering animations"
   return ()
